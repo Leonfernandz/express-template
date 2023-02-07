4bf46cab5f98da6adfc8e1b370ae26627db65a51
Leonfernandz <118799876+Leonfernandz@users.noreply.github.com>
2023-02-07 22:57:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=> {
-    res.status(200).send(<h1>Welcome to API</h1)
+    res.status(200).send('<h1>Welcome to API</h1>')
 });
 
 app.listen(PORT, ()=> console.log(`Running API ${PORT}`));
